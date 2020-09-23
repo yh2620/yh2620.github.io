393c843c230463d132c15aa55646fc0ce115ca97
yh2620 <57832387+yh2620@users.noreply.github.com>
2020-09-23 22:43:48 +0900

    Update and rename js.js to js-dc.js

---
+++ script/js-dc.js
@@ -0,0 +1,17 @@
+    function goDcinside(){
+    var where = prompt("이동할 갤러리의 id를 입력하세요");
+    if (where == null) {
+        alert("[취소]를 누르셨습니다.");
+        }
+    else {
+        location.href = "https://gall.dcinside.com/" + where;
+        }
+    }
+    function searchDC(){
+    var q = prompt("검색어를 입력하세요");
+    if (q == null){
+        }
+    else {
+         location.href = "https://search.dcinside.com/post/sort/accuracy/q/" + q;
+        }
+    }
