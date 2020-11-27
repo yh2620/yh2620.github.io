4e3b764df4b3c22397b2a36864f4ecbc5b3d5b2f
yh2620 <57832387+yh2620@users.noreply.github.com>
2020-11-27 20:18:46 +0900

    Update index-dc.js

--- script/index-dc.js
+++ script/index-dc.js
@@ -1,4 +1,4 @@
-    function goDcinside(){
+function goDcinside(){
     var gallid = prompt("이동할 갤러리의 id를 입력하세요");
     if (gallid == null) {
         }
@@ -6,7 +6,7 @@
         window.open("https://gall.dcinside.com/"+gallid);
         }
     }
-    function searchDC(){
+function searchDC(){
     var keyword = prompt("검색어를 입력하세요");
     if (keyword == null){
         }
@@ -14,3 +14,9 @@
          window.open("https://search.dcinside.com/post/sort/accuracy/q/"+keyword);
         }
     }
+function redditPopup(){
+    var subRedditName = prompt("서브레딧의 이름을 입력하세요");
+    if (subRedditName != null){
+        window.open("https://reddit.com/r/"+subredditname,"ㅇㅇ","width=800,height=800")
+    }
+}
