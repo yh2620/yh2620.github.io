9e897c9295006ea3fc5206611ca8b56e1fb974f9
yh2620 <57832387+yh2620@users.noreply.github.com>
2021-02-11 14:11:58 +0900

    Update index-dc.js

--- script/index-dc.js
+++ script/index-dc.js
@@ -1,28 +1,30 @@
 function goDcinside(){
-    var gallid = prompt("이동할 갤러리의 id를 입력하세요");
-    if (gallid == null) {
-        }
-    else {
+    var gallid = prompt("이동할 갤러리의 ID를 입력하세요");
+    if (gallid != null){
         window.open("https://gall.dcinside.com/"+gallid);
         }
-    }
+}
 function searchDC(){
     var keyword = prompt("검색어를 입력하세요");
-    if (keyword == null){
-        }
-    else {
+    if (keyword != null){
          window.open("https://search.dcinside.com/post/sort/accuracy/q/"+keyword);
         }
-    }
+}
 function goReddit(){
-    var subRedditName = prompt("서브레딧의 ID를 입력하세요");
+    var subRedditName = prompt("서브레딧의 이름을 입력하세요");
     if (subRedditName != null){
-        window.open("https://reddit.com/r/" + subRedditName)
+        window.open("https://reddit.com/r/" + subRedditName);
     }
 }
 function redditPopup(){
-    var subRedditName = prompt("서브레딧의 ID를 입력하세요");
+    var subRedditName = prompt("서브레딧의 이름을 입력하세요");
     if (subRedditName != null){
-        window.open("https://reddit.com/r/" + subRedditName,"reddit","width=800,height=1000")
+        window.open("https://reddit.com/r/" + subRedditName,"reddit","width=800,height=1000");
+    }
+}
+function redditsearch(){
+    var keyword = prompt("검색어를 입력하세요");
+    if (keyword != null){
+        window.open("https://reddit.com/search/?q=" + keyword);
     }
 }
